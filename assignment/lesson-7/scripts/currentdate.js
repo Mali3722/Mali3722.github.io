a6cb66cc594ca8ee33cf614d41c46c94c16eaf3f
Mali3722 <phi15019@byui.edu>
2019-06-07 22:24:26 -0600

    update10

---
+++ assignment/lesson-7/scripts/currentdate.js
@@ -0,0 +1,8 @@
+funtion toggleMenu() {
+    document.getElementsByClassName("navigation")
+    [0].classList.toggle("responsive");
+}
+{
+    var date=today();
+    document.getElementsById("todaysdate").innerHTML=today;
+}
