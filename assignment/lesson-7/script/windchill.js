d6577ebe335004dff081949c5e8b29348e9a12b3
Mali3722 <phi15019@byui.edu>
2019-06-08 16:40:36 -0600

    update25

---
+++ assignment/lesson-7/script/windchill.js
@@ -0,0 +1,6 @@
+var x =parseFloat(docuent.getElementById("chill").innerHTML);
+var y = parseFloat(document.getElementById("temp".innerHTML);
+
+var result = () {
+document.getElementById("?").innerHTML = result;
+}
