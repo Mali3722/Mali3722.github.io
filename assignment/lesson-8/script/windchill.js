08549d6f77b2d5db5ab0ed3f712b3cc2d1d0257c
Mali3722 <phi15019@byui.edu>
2019-06-13 20:57:17 -0600

    update11

--- assignment/lesson-8/script/windchill.js
+++ assignment/lesson-8/script/windchill.js
@@ -3,5 +3,5 @@ var s = parseFloat(document.getElementById("speed").innerHTML);
 
 var e = Math.pow(s, 0.16);
 var f = 35.74 + 0.6215 * t - 35.75 * e + 0.4275 * t * e;
-		
+f= Math.round(f);
 document.getElementById("result").innerHTML = f;
