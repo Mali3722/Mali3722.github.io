c3b1127fcec3841bf023fb59dfbeceac9c64bf7f
Mali3722 <phi15019@byui.edu>
2019-06-15 09:56:41 -0600

    update80

--- assignment/lesson-8/script/adjustrating.js
+++ assignment/lesson-8/script/adjustrating.js
@@ -1,3 +1,8 @@
-function adjustRating(rating) {
-    document.getElementById("ratingvalue").innerHTML = rating;
+var slider = document.getElementById("myRange");
+var output = document.getElementById("value");
+
+output.innerHTML = slider.value;
+
+slider.oniput = function() {
+    output.innerHTML = this.value;
 }
