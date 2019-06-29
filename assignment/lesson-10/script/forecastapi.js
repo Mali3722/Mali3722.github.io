40d24bcacee3c4963693c36ceacba0e5234ad85e
Mali3722 <Phi15019@byui.edu>
2019-06-29 13:46:39 -0600

    ks

--- assignment/lesson-10/script/forecastapi.js
+++ assignment/lesson-10/script/forecastapi.js
@@ -7,12 +7,25 @@ weatherObject.onload = function () {
     let weatherInfo = JSON.parse(weatherRequest.responseText);
     console.log(weatherInfo);
 
-    
-    
-    let icon = "http://openweathermap.org/img/wn/" + weatherInfo.weather[0].icon + ".png"
+    var forecast = 1;
+    for (i = 0; i < weatherInfo; i++) {
+        if (weatherInfo.list[i].dt_txt.includes('18:00:00')) {
+            if (forcast <= 5) {
 
-    let description = weatherInfo.weather[0].description;
 
-    document.getElementById('cc-img').setAttribute('src', icon);
-    document.getElementById('current').setAttribute('alt', description);
+                var day = "day" + frocast;
+
+
+
+                let icon = "http://openweathermap.org/img/wn/" + weatherInfo.weather[0].icon + ".png"
+
+                let description = weatherInfo.weather[0].description;
+
+                document.getElementById('cc-img').setAttribute('src', icon);
+                document.getElementById('current').setAttribute('alt', description);
+
+                forecast++;
+            }
+        }
+    }
 }
