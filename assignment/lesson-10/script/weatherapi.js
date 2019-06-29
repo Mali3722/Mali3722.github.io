effcd8f7218a181ff8b01b40f57536ae2605c464
Mali3722 <Phi15019@byui.edu>
2019-06-29 10:17:22 -0600

    kif

---
+++ assignment/lesson-10/script/weatherapi.js
@@ -0,0 +1,11 @@
+let weatherRequest = new XMLHttpRequest();
+let apiURLstring ='https://api.openweathermap.org/data/2.5/weather?id=5061036&units=imperial&APPID=e2b7cdf510dc1ed38f09a77e6684cf29';
+weatherRequest.open('Get', apiURLstring, true);
+weatherRequest.send();
+
+weatherRequest.onload = function(){
+    let weatherData = JSON.parse(weatherRequest.responseText);
+    console.log(weatherData);
+
+    document.getElementById('cc-temp').innerHTML = weatherData.main.temp;
+}
