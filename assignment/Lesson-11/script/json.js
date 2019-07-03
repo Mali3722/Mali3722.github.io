749e6365151adbd803a856b09623507a20354960
Mali3722 <Phi15019@byui.edu>
2019-07-02 20:44:12 -0600

    kl

--- assignment/Lesson-11/script/json.js
+++ assignment/Lesson-11/script/json.js
@@ -45,17 +45,6 @@ function showData(jsonObj) {
         }
     }
 }
-            var myList = document.createElement('ul');
-            myH2.textContent = 'Events: ';
-            myPara5.textContent = 'Events ' + towns[i].events;
-            myArticle.appendChild(myPara5); 
-
-            var events = towns[i].events;
-            for (var j = "0"; j < towns.length; j++) {
-            var listItem = document.createElement('li');
-            listItem.textContent = events[j];
-            var myList =document.getElementById(towns[i].events)
-            myList.appendChild(listItem);
-}
+           
 
 
